cf28e5aeb01982b2562fe2d36f5edbae3b3f4a8a
SpaceandAllTheStars <spaceandallthestars@gmail.com>
2026-04-08 08:33:17 -0600

    created new project cuz uhmm yeah

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,30 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
+import swervelib.math.Matter;
+
+public final class Constants
+{
+
+public static final double ROBOT_MASS = 100 * 0.453592;
+  public static final Matter CHASSIS    = new Matter(new Translation3d(0, 0, Units.inchesToMeters(8)), ROBOT_MASS);
+  public static final double LOOP_TIME  = 0.13;
+  public static final double MAX_SPEED  = Units.feetToMeters(14.5);
+
+  public static final class DrivebaseConstants
+  {
+  }
+
+  public static class OperatorConstants
+  {
+    public static final double DEADBAND        = 0.1;
+    public static final double LEFT_Y_DEADBAND = 0.1;
+    public static final double RIGHT_X_DEADBAND = 0.1;
+    public static final double TURN_CONSTANT    = 6;
+  }
+}
